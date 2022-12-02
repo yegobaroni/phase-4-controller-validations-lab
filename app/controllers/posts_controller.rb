c8cf40956395c75cc7a07f8ca333bf1b939ad43a
yego <yegokipkemoi254@gmail.com>
2022-12-02 19:57:46 +0300

    Done

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,9 +8,7 @@ class PostsController < ApplicationController
 
   def update
     post = Post.find(params[:id])
-
-    post.update(post_params)
-
+    post.update!(post_params)
     render json: post
   end
 
